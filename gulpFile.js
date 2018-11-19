84a18ac47a8d19a7cf5185338511f77bc9b555f6
Kurt Lourens <hi@kurtlourens.com>
2018-11-19 22:45:50 +0200

    addded robots.txt

--- gulpFile.js
+++ gulpFile.js
@@ -12,7 +12,7 @@ var browserSync = require('browser-sync').create();
 
 
 var inputPaths = {
-  misc: ["./favicon.ico", "./.htaccess", "./sitemap.xml", "./docs", "./Images"],
+  misc: ["./docs", "./Images", "./favicon.ico", "./.htaccess", "./sitemap.xml", "./robots.txt"],
   assets: "./assets/**/*.*",
   scss: "./sass/**/*.scss",
   css: "./assets/css/*.css",
