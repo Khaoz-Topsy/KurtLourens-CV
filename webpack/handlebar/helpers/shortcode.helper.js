ab6788193b7915cf8adcdd5abf5398e5f3a5c280
Kurt Lourens <hi@kurtlourens.com>
2025-05-05 20:51:07 +0200

    🔖 4.4.0

--- webpack/handlebar/helpers/shortcode.helper.js
+++ webpack/handlebar/helpers/shortcode.helper.js
@@ -1,32 +1,39 @@
 function calculateYearsOfWork() {
-    const now = Date.now();
-    const startedWorkInMilli = new Date('2017-01-01');
-    const nowInMilli = new Date(now);
-    const differenceInMilli = (nowInMilli - startedWorkInMilli);
-    const millisecondsPerYear = 365 * 24 * 60 * 60 * 1000;
+  const now = Date.now();
+  const startedWorkInMilli = new Date('2017-01-01');
+  const nowInMilli = new Date(now);
+  const differenceInMilli = nowInMilli - startedWorkInMilli;
+  const millisecondsPerYear = 365 * 24 * 60 * 60 * 1000;
 
-    let result = Math.floor(differenceInMilli / millisecondsPerYear).toFixed(0);
-    const monthsInYears = (differenceInMilli / millisecondsPerYear) % 1;
-    if (monthsInYears > 7.5) result = +result + 1;
-    else if (monthsInYears > 0.1) result += '+';
-    result += ' years';
-    return result;
+  let result = Math.floor(differenceInMilli / millisecondsPerYear).toFixed(0);
+  const monthsInYears = (differenceInMilli / millisecondsPerYear) % 1;
+  if (monthsInYears > 7.5) result = +result + 1;
+  else if (monthsInYears > 0.1) result += '+';
+  result += ' years';
+  return result;
 }
 
 module.exports = function (context, options) {
-    const shortCodeMap = {
-        '[entelect]': '<a href="http://www.entelect.co.za?ref=kurtlourens.com" class="color-entelect" target="_blank" rel="noopener" title="Entelect" alt="Entelect">Entelect</a>',
-        '[redRubyIt]': '<a href="http://redrubyit.co.za?ref=kurtlourens.com" class="color-ruby" target="_blank" rel="noopener" title="Red Ruby IT" alt="Red Ruby IT">Red Ruby IT</a>',
-        '[flutter]': '<a href="https://flutter.dev/?ref=kurtlourens.com" class="color-flutter" target="_blank" rel="noopener" title="Flutter" alt="Flutter">Flutter</a>',
-        '[workDuration]': calculateYearsOfWork(),
-        '[blog]': '<a href="https://blog.kurtlourens.com?ref=kurtlourens.com" target="_blank" rel="noopener noreferrer" title="Kurt\'s blog">blog</a>',
-        '[blogCvArticle]': '<a href="https://blog.kurtlourens.com/kurtlourens-com-redesigned/?ref=kurtlourens.com" target="_blank" rel="noopener noreferrer" title="Kurt\'s blog">click here to read it</a>',
-    };
+  const shortCodeMap = {
+    '[entelect]':
+      '<a href="http://www.entelect.co.za?ref=kurtlourens.com" class="color-entelect" target="_blank" rel="noopener" title="Entelect" alt="Entelect">Entelect</a>',
+    '[redRubyIt]':
+      '<a href="http://redrubyit.co.za?ref=kurtlourens.com" class="color-ruby" target="_blank" rel="noopener" title="Red Ruby IT" alt="Red Ruby IT">Red Ruby IT</a>',
+    '[flutter]':
+      '<a href="https://flutter.dev/?ref=kurtlourens.com" class="color-flutter" target="_blank" rel="noopener" title="Flutter" alt="Flutter">Flutter</a>',
+    '[workDuration]': calculateYearsOfWork(),
+    '[blog]':
+      '<a href="https://blog.kurtlourens.com?ref=kurtlourens.com" target="_blank" rel="noopener noreferrer" title="Kurt\'s blog">blog</a>',
+    '[blogCvArticle]':
+      '<a href="https://blog.kurtlourens.com/kurtlourens-com-redesigned/?ref=kurtlourens.com" target="_blank" rel="noopener noreferrer" title="Kurt\'s blog">click here to read it</a>',
+    '[raboWrapped]':
+      '<a href="https://nos.nl/artikel/2501584-na-spotify-nu-ook-rabobank-wrapped-maar-wel-erg-confronterend?ref=kurtlourens.com" target="_blank" rel="noopener noreferrer" title="Rabobank Terugblik">Rabobank Terugblik</a>',
+  };
 
-    let innerHTML = context;
-    for (const shortCodeMapProp in shortCodeMap) {
-        const rex = new RegExp(`\\${shortCodeMapProp}`, 'g');
-        innerHTML = innerHTML.replace(rex, shortCodeMap[shortCodeMapProp]);
-    }
-    return innerHTML;
+  let innerHTML = context;
+  for (const shortCodeMapProp in shortCodeMap) {
+    const rex = new RegExp(`\\${shortCodeMapProp}`, 'g');
+    innerHTML = innerHTML.replace(rex, shortCodeMap[shortCodeMapProp]);
+  }
+  return innerHTML;
 };
